3b435d28d24b283079c944e13cf3045fde0d73e5
Sidinei Silva <sidinei.silva02@gmail.com>
2021-12-26 19:25:33 -0300

    feat: :sparkles: Include patch todo to done
    Incluindo chamada para atualizar propriedade done de todo

--- src/index.js
+++ src/index.js
@@ -90,7 +90,19 @@ app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
 });
 
 app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: "Todo not found" });
+  }
+
+  todo.done = true;
+
+  return response.status(200).json(todo);
+
 });
 
 app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
