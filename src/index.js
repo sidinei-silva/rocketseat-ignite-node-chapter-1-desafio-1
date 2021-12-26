9affd9f480a307fe932a289aab276abf85176941
Sidinei Silva <sidinei.silva02@gmail.com>
2021-12-26 19:26:32 -0300

    feat: :sparkles: Include delete todo
    Incluindo deleção de todo

--- src/index.js
+++ src/index.js
@@ -106,7 +106,19 @@ app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
 });
 
 app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todoIndex = user.todos.findIndex((todo) => todo.id === id);
+
+  if (todoIndex === -1) {
+    return response.status(404).json({ error: "Todo not found" });
+  }
+
+  user.todos.splice(todoIndex, 1);
+
+  return response.status(204).json();
+
 });
 
 module.exports = app;
